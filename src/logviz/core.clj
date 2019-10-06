60126f17072cc94e19161ba7c03dc8c53c79f1d4
Ahmad Nazir Raja <ahmadnazir@gmail.com>
2019-10-06 02:02:03 +0200

    Generating a histogram

--- src/logviz/core.clj
+++ src/logviz/core.clj
@@ -20,10 +20,75 @@
   "Add value to the bucket"
   [coll bucket line]
   (let [[k v] (last coll)]
-    (cond (empty? coll) (vector [bucket 1])
-          (= k bucket)  (conj (drop-last coll) [bucket (inc v)])
+    (cond (empty? coll) [ [bucket 1] ]
+          (= k bucket)  (conj (vec (drop-last coll)) [bucket (inc v)])
           :else         (conj coll [bucket 1])
           )))
+
+;; utils
+;; -----
+
+(defn print!
+  "Eagerly print all the values in a collection"
+  [collection]
+  (loop [rows collection]
+    (cond (empty? rows) (println "")
+          :else (do (println (first rows))
+                    (recur (rest rows))
+                    )))
+  )
+
+(defn tee!
+  "Print and return"
+  [col]
+  (do
+    (print! col)
+    col
+    )
+  )
+
+;; graph
+;; ----
+
+(defn pivot
+  "Pivot a matrix"
+  [col]
+  (apply map list col))
+
+(defn flip
+  "Flip a matrix"
+  [col]
+  (map reverse col))
+
+(defn graph
+  "Graph! Take the values and create a histogram. [[k v]] -> v -> number of chars -> pivot -> graph"
+  [col]
+  (let [values (map second col)
+        height (apply max values)
+        ]
+    (->> values
+         (map (fn [x] (concat (repeat x "#") (repeat (- height x) " ")))) ;; use chars for the histogram
+         flip                                                ;; mirror / flip i.e. 180 degrees
+         pivot                                             ;; pivot / rotate 90 degrees
+         (map (fn [c] (str (apply str c))))                           ;; string representation of a graph
+         ))
+  )
+
+
+;; TODO
+;;
+;; Sporadic:
+;; - find the min dist?
+;;
+;; Events really close together:
+;; - based on the distribution, group the keys and graph
+;;
+;; Adjust the height:
+;; - If there are too many items in a bucket, then they need to be normalized
+;;
+;; Different types of items:
+;; - Right now I am only counting the items - not counting the different types of items
+
 
 (defn -main [& args]
   (loop [m [] lines (line-seq (java.io.BufferedReader. *in*))]
@@ -31,6 +96,9 @@
           time (t/time-now)
           m (append m (bucket) line)
           ]
-      (println m)
+      ;; The graph m has some lazy component that I can't figure out - need to
+      ;; explicity get all the rows by calling `first`. Simply doing a (map
+      ;; println (graph m)) won't print anything
+      (print! (graph m))
       (recur m (rest lines))))
   )
--- test/logviz/core_test.clj
+++ test/logviz/core_test.clj
@@ -1,4 +1,4 @@
-(ns logviz.test.core-test
+(ns logviz.core-test
   (:require [logviz.core :as sut]
             [clojure.test :refer :all]))
 
@@ -13,8 +13,8 @@
 (deftest add-to-collection-with-one-element
   (testing "Add to collection with one element"
     (is (=
-         (sut/append [["key" 1]] "key" "value")
-         [["key" 2]]
+         (sut/append [["key" 4]] "key" "value")
+         [["key" 5]]
          ))
     ))
 
@@ -25,4 +25,19 @@
          [["key" 2] ["new-key" 1]]
          ))
     ))
+
+(deftest graph
+  (testing "Add to collection with one element and create a new bucket"
+    (is (=
+         (sut/graph [["_" 1] ["_" 2] ["_" 1] ["_" 3]])
+         ["   x"
+          " x x"
+          "xxxx"]
+         ))
+    ))
+
+
+(->> (sut/graph [["_" 1] ["_" 2] ["_" 1] ["_" 3]])
+     (map println)
+     )
 
